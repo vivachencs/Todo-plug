ed88ad3f3c60b9e015996885ecbcfb5010610c3f
ChenX <vivachencs@gmail.com>
2017-02-21 16:49:24 +0800

    主体完成

---
+++ js/todolist.js
@@ -0,0 +1,43 @@
+// 创建 Angular 对象
+var todoApp = window.angular.module("todoApp", [])
+
+// 对象的控制器参数
+todoApp.controller('todolistcontroller', ['$scope', function($scope){
+    $scope.text = ''
+
+    // 默认清单
+    $scope.todolist = [{
+        text: 'hello world',
+        done: false
+    }]
+
+    // 增添任务
+    $scope.add = function() {
+
+        //获取文本
+        var text = $scope.text.trim()
+        if (text) {
+            $scope.todolist.push({
+                text: text,
+                done: false
+            })
+            $scope.text = ''
+        }
+    }
+
+    //删除任务
+    $scope.delete = function(todo) {
+        var index = $scope.todolist.indexOf(todo)
+
+        $scope.todolist.splice(index, 1)
+    }
+
+    //统计任务总量
+    $scope.doneCount = function() {
+        var temp = $scope.todolist.filter(function(item){
+            return item.done
+        })
+        //任务个数
+        return temp.length
+    }
+}])
